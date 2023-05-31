81fb44625fc3c4fb763c996d102ed61b9809a82b
Kaya Sertel <50380412+KayaSrtl@users.noreply.github.com>
2023-05-31 16:15:06 +0300

    Update sudokuscript.js

--- sudokuscript.js
+++ sudokuscript.js
@@ -46,7 +46,12 @@ function getSudoku(board) {
 solveSudoku(board);
 }
 
+function resetSudoku(board) {
+	for(let i = 1; i <= 9; i++)
+		for(let j = 1; j <= 9; j++) 
+			$("#tr" + i).children("#td" + j).text("");
 
+}
 // Sudoku çözme işlevi
 function solveSudoku(board) {
   const emptySpot = findEmptySpot(board);
